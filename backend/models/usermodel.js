c8ea646805b8631158feb713114dd8b162422e09
robin6oy <robinvarghese0k@gmail.com>
2023-06-12 22:25:04 +0530

    comit

---
+++ backend/models/usermodel.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var userModelSchema = mongoose.Schema({
+    donarname:{
+        type:String
+    },
+    number:{
+        type:String
+    }, 
+    accessno:{
+        type:Number
+    },
+    key:{
+        type:String
+    },
+    role:{
+        type:String,
+        default:"User"
+    },
+    status:{
+        type:String,
+        default:"Active"
+    }
+}, {
+    timestamps: { createdAt: 'create_date', updatedAt: 'update_date' }
+});
+
+var userModel = module.exports = mongoose.model('userModel', userModelSchema);
+module.exports.get = function (callback, limit) {
+    userModel.find(callback).limit(limit);
+}
---
+++ backend/routes/userroute.js
@@ -0,0 +1,177 @@
+const express = require('express')
+const router = express()
+const Misc = require('../controllers/misc')
+const UserModel = require('../models/usermodel')
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const tokenModel = require('../models/tokenmodel')
+var defaults = require('../defaults')
+
+const { UserAgent } = require('express-useragent');
+const adminauth = require('../middilewares/securityauth');
+
+
+ 
+
+//Admin Registration
+
+router.post('/donarregistration', async (req, res) => {
+  try {
+    var {donarname, donarnumber,accessnumber, key } = req.body
+    if (Misc.isnullorempty(donarname)) {
+      res.status(200).json({
+        status: false,
+        msg: "guard name not found"
+      });
+      return;
+    }
+    if (Misc.isnullorempty(donarnumber)) {
+      res.status(200).json({
+        status: false,
+        msg: "donarnumber not found"
+      });
+      return;
+    }
+    if (Misc.isnullorempty(accessnumber)) {
+      
+      res.status(200).json({
+        status: false,
+        msg: "access number not found"
+      });
+      return;
+    }
+    if (Misc.isnullorempty(key)) {
+      res.status(200).json({
+        status: false,
+        msg: "key not found"
+      });
+      return;
+    }
+    var alreadyExists = await UserModel.find({ accessno: accessnumber, status: "Active" })
+    if (!Misc.isnullorempty(alreadyExists)) {
+      res.status(200).json({
+        status: false,
+        msg: "Access Number Already Registered"
+      });
+      return;
+    } 
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(key, salt);
+    var user = new UserModel()
+    user.donarname = donarname;
+    user.gateno = donarnumber;
+    user.accessno = accessnumber;
+    user.key = hash;
+    user.role ="Security"
+    await user.save()
+    res.status(200).json({
+      status: true,
+      msg: " Registration sucessfull "
+    });
+    return;
+  } catch (error) {
+    console.log(error)
+
+  }
+})
+
+
+
+
+
+//Guard Login
+
+
+router.post('/Login',async (req, res) => {
+    try {
+      var { accessnumber, key, } = req.body;
+      if (Misc.isnullorempty(accessnumber)) {
+        res.status(200).json({
+          status: false,
+          msg: "accessno not found"
+        });
+        return;
+      }
+      if (Misc.isnullorempty(key)) {
+        res.status(200).json({
+          status: false,
+          msg: "key not found"
+        });
+        return;
+      }
+      var alreadyExists = await UserModel.findOne({ accessno: accessnumber, status: "Active" })
+      if (Misc.isnullorempty(alreadyExists)) {
+        res.status(200).json({
+          status: false,
+          msg: "invalid access"
+        });
+        return;
+      }
+      // console.log(key,alreadyExists.key)
+      var check = await bcrypt.compare(key,alreadyExists.key)
+      if(!check){
+        res.status(200).json({
+          status: false,
+          msg: "Incorrect key"
+        });
+        return;
+      }
+      alreadyExists = JSON.parse(JSON.stringify(alreadyExists))
+      delete alreadyExists.key;
+      var token=jwt.sign({id: alreadyExists._id/* user:alreadyExists */},defaults.secret)
+      var tokendata=new tokenModel()
+      tokendata.userid=alreadyExists._id
+      tokendata.token=token
+      await tokendata.save()
+        res.status(200).json({
+          status: true,
+          msg: "Login sucess",
+          data: alreadyExists,
+          token:token
+        });
+        return;
+    } catch (error) {
+      console.log(error)
+    }
+  });
+
+
+  //Logout    
+  
+  router.get('/logout',adminauth, async (req, res) => {
+    try{
+      var token = req.body.token || req.query.token || req.headers.token;
+  
+    if (Misc.isnullorempty(token)) {
+      res.status(200).json({
+        status: false,
+        msg: " token not found"
+      });
+      return;
+    }
+    var tok = await tokenModel.findOneAndRemove({ userid:req.user.id,token:token,status:"Active" })
+  
+      res.status(200).json({
+        status: true,
+        msg: "logout sucess"
+      });
+      return;
+      }
+       catch (error) {
+        console.log(error)
+      res.status(500).json({
+        status: false,
+        msg: " Something went wrong"
+    })
+  return;
+  }
+      
+  }
+  
+  )
+  
+
+
+
+
+module.exports = router;
